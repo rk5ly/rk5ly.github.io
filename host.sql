c1aadae4dc07d8f0a78b0d9d6524ccf9b86513cd
rk5ly <151930238+rk5ly@users.noreply.github.com>
2025-02-05 11:56:37 +0700

    Add files via upload

---
+++ host.sql
@@ -0,0 +1,24 @@
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `hostf`
+--
+
+CREATE TABLE IF NOT EXISTS `hostf` (
+  `id` int(200) NOT NULL AUTO_INCREMENT,
+  `nick` varchar(200) NOT NULL,
+  `desc` varchar(200) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `file` varchar(200) NOT NULL,
+  `count` int(200) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
