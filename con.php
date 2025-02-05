f3649119507076c54c07aa75ce50435556b7f61b
rk5ly <151930238+rk5ly@users.noreply.github.com>
2025-02-05 11:58:09 +0700

    Update con.php

--- con.php
+++ con.php
@@ -1,9 +1,9 @@
 <?php
 
-$dbh='localhost'; //db host
-$dbn='your'; //db name
-$dbu='your'; //db user
-$dbp='your'; //db pass
+$dbh='https://rk5ly.github.io'; //db host
+$dbn='admin'; //db name
+$dbu='admin'; //db user
+$dbp='admin'; //db pass
 
 $db = mysql_connect("$dbh","$dbu","$dbp") or die("Could not connect: " . mysql_error());
 mysql_select_db("$dbn", $db) or die(mysql_error());
