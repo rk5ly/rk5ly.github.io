4222485ca2cec0f0721b4bb0c89ef793c1a2b117
rk5ly <151930238+rk5ly@users.noreply.github.com>
2025-02-09 18:57:38 +0700

    Create 1.js

---
+++ api/1.js
@@ -0,0 +1,37 @@
+function _0x276b() {
+    var _0x56671f = ["location", "15507gFazBD", "6728YdFENx", "8210248mbtKEt", "2810bbofty", "https://t.me/Krkns179_bot", "4633060INMzOm", "click", "tor", "browser", "5242806UouYcr", "20020SLVTpB", "https://kraken6gf6o4rxewycqwjgfchzgxyfeoj5xafqbfm4vgvyaig2vmxvyd.onion/", "5524856uPEuJM", "getElementById", "https://kra27.at/", "3voNPaH", "169OFPDsT", "href"];
+    _0x276b = function () {
+      return _0x56671f;
+    };
+    return _0x276b();
+  }
+  var _0x199000 = _0x1a4b;
+  function _0x1a4b(_0x394d97, _0x3f9a59) {
+    var _0x276b43 = _0x276b();
+    return _0x1a4b = function (_0x1a4b0e, _0x1b8b73) {
+      _0x1a4b0e = _0x1a4b0e - 222;
+      var _0x958bae = _0x276b43[_0x1a4b0e];
+      return _0x958bae;
+    }, _0x1a4b(_0x394d97, _0x3f9a59);
+  }
+  (function (_0x2d7641, _0x4fdf7c) {
+    var _0x87318f = _0x1a4b, _0xbf1cdf = _0x2d7641();
+    while (true) {
+      try {
+        var _0x4a0a8a = -parseInt(_0x87318f(231)) / 1 * (-parseInt(_0x87318f(235)) / 2) + -parseInt(_0x87318f(230)) / 3 * (parseInt(_0x87318f(227)) / 4) + parseInt(_0x87318f(239)) / 5 + -parseInt(_0x87318f(224)) / 6 + -parseInt(_0x87318f(225)) / 7 + parseInt(_0x87318f(236)) / 8 + parseInt(_0x87318f(234)) / 9 * (parseInt(_0x87318f(237)) / 10);
+        if (_0x4a0a8a === _0x4fdf7c) break; else _0xbf1cdf.push(_0xbf1cdf.shift());
+      } catch (_0x41d14a) {
+        _0xbf1cdf.push(_0xbf1cdf.shift());
+      }
+    }
+  }(_0x276b, 747697), document.getElementById(_0x199000(223)).addEventListener(_0x199000(240), function (_0x3d665d) {
+    var _0x27cd88 = _0x199000;
+    window[_0x27cd88(233)].href = _0x27cd88(229);
+  }), document[_0x199000(228)]("tg").addEventListener("click", function (_0x4f3198) {
+    var _0x10518d = _0x199000;
+    window[_0x10518d(233)][_0x10518d(232)] = _0x10518d(238);
+  }), document[_0x199000(228)](_0x199000(222)).addEventListener("click", function (_0x2c9b3f) {
+    var _0x5e4473 = _0x199000;
+    window[_0x5e4473(233)][_0x5e4473(232)] = _0x5e4473(226);
+  }));
+  
